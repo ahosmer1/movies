ef7f715281ba2c0d4c79f486a29d8f1c5a0b99af
Allan Hosmer <ahosmer1@gmail.com>
2013-11-21 16:24:02 -0800

    add new sort of working, still nothing on edit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,6 +16,15 @@ class MoviesController < ApplicationController
     @movies = Movie.find(params[:id])
   end
   def new
-    @movies = Movies.new
+    @movies = Movie.new
+  end
+  def create
+    @movies = Movie.new( safe_movie_params )
+    @movies.save
+    redirect_to root_path
+  end
+  private
+  def safe_movie_params
+    return params.require(:movie).permit(:title, :description, :year_released, :image)
   end
 end
